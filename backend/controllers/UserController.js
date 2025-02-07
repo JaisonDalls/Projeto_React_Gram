a642ecb22f424945221d6546bbaf167057a92c39
Jaison Rodrigo Dallabrida <jaisondalls@gmail.com>
2025-02-07 01:28:19 -0300

    feat: User insertation and validations then.

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -12,7 +12,36 @@ const generateToken = (id) => {
 
 //Register user and sign in
 const register = async (req, res) => {
-    res.send("Registro Page");
+    const {name, email, password} = req.body;
+
+    //check if user exists
+    const user = await User.findOne({email});
+    if(user){
+        res.status(422).json({errors: ["Já existe este e-mail cadastrado em nosso sistema!"]});
+        return;
+    }
+
+    //Generate password hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    //Create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    });
+
+    //Check if user was created succesfully, return token
+    if(!newUser){
+        res.status(422).json({errors: ["Não foi possível cadastrar usuário!"]});
+        return;
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    });
 }
 
 module.exports = {
