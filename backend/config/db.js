3cf0393eb3baf63e24051c3c684dbe7f4ffd2ebc
Jaison Rodrigo Dallabrida <jaisondalls@gmail.com>
2025-02-06 22:22:19 -0300

    feat: Configurated the db connection from MongoDB.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,20 @@
 const mongoose = require('mongoose');
 
 //connection
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+const connect = async () => {
+    try {
+        const dbConnect = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.uf18h.mongodb.net/`);
+        console.log('Conexão no banco estabelecida!');
+        
+        return dbConnect;        
+    } catch (error) {
+        console.log(error);        
+    }
+};
+
+connect();
+
+module.exports = connect;
