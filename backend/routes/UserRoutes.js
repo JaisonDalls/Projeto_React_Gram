91c1b8822a60c68b82ed4aa12b0bfa5e3047aec2
Jaison Rodrigo Dallabrida <jaisondalls@gmail.com>
2025-02-09 01:03:51 -0300

    feat: Generated user upload middleware and user upload function.

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,30 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-//Controller 
-const {register, login, getCurrentUser} = require('../controllers/UserController');
+//Controller
+const {
+  register,
+  login,
+  getCurrentUser,
+  update
+} = require("../controllers/UserController");
 
 //Middlewares
-const validate = require('../middlewares/handleValidation');
-const {userCreateValidation, loginValidation} = require('../middlewares/userValidations');
-const authGuard = require('../middlewares/authGuard');
+const validate = require("../middlewares/handleValidation");
+const {
+  userCreateValidation,
+  loginValidation,
+  userUpdateValidation,
+} = require("../middlewares/userValidations");
+const authGuard = require("../middlewares/authGuard");
+const imageUpload = require("../middlewares/imageUpload");
 
 //Routes
-router.post("/register",
-    userCreateValidation(),
-    validate, 
-    register
-);
+router.post("/register", userCreateValidation(), validate, register);
 
-router.post("/login",
-    loginValidation(),
-    validate,
-    login    
-);
+router.post("/login", loginValidation(), validate, login);
 
-router.get("/profile",
-    authGuard,
-    getCurrentUser
-);
+router.get("/profile", authGuard, getCurrentUser);
+
+router.put('/', authGuard, userUpdateValidation(), validate, imageUpload.single("profileImage"), update)
 
 module.exports = router;
