6eaa08446e394a9fd4bb8144e5dd6dd91db3a35d
Jaison Rodrigo Dallabrida <jaisondalls@gmail.com>
2025-02-22 01:32:47 -0300

    feat: Created initial frontend components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,32 @@
-import './App.css';
-import Home from './pages/Home/Home';
-import Register from './pages/Auth/Register';
-import Login from './pages/Auth/Login';
+import "./App.css";
+
+//Pages
+import Home from "./pages/Home/Home";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+
+//Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 //Router
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-   <div className='App'>
+    <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={<Login/>} />
-          <Route path='/register' element={<Register/>} />
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
-   </div>
+    </div>
   );
 }
 
